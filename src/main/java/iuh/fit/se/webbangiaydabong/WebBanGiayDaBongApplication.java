e1a2601e8d52dd64cd044da7dac73da91adca23e
TranLoi2002 <121424271+TranLoi2002@users.noreply.github.com>
2025-01-07 23:48:22 +0700

    first commit

---
+++ src/main/java/iuh/fit/se/webbangiaydabong/WebBanGiayDaBongApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.webbangiaydabong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanGiayDaBongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanGiayDaBongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iuh/fit/se/webbangiaydabong/WebBanGiayDaBongApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.webbangiaydabong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanGiayDaBongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
